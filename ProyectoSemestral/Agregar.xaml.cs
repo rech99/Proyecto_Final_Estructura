66b1d8d81aac51c86b3e1f84b1368b10ee54e032
rech99 <rech_99@hotmail.com>
2019-11-13 09:39:31 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoSemestral/Agregar.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoSemestral
+{
+    /// <summary>
+    /// Lógica de interacción para Agregar.xaml
+    /// </summary>
+    public partial class Agregar : UserControl
+    {
+        public Agregar()
+        {
+            InitializeComponent();
+        }
+
+        private void Cb_pelicula_Checked(object sender, RoutedEventArgs e)
+        {
+            grid_2.Children.Clear();
+            grid_2.Children.Add(new Peliculas());
+        }
+
+        private void Cb_serie_Checked(object sender, RoutedEventArgs e)
+        {
+            grid_2.Children.Clear();
+            grid_2.Children.Add(new Series());
+        }
+    }
+}
---
+++ ProyectoSemestral/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoSemestral
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Nuevo_Click(object sender, RoutedEventArgs e)
+        {
+            Grid1.Children.Clear();
+            Grid1.Children.Add(new Agregar());
+            Nuevo.Visibility = Visibility.Hidden;
+            AZ.Visibility = Visibility.Hidden;
+            Za.Visibility = Visibility.Hidden;
+            Año_mayor.Visibility = Visibility.Hidden;
+            Año_menor.Visibility = Visibility.Hidden;
+            
+        }
+    }
+}
